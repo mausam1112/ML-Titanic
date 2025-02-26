7a9868b76413e4fadb35e1983f68f381c517925e
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-27 01:49:22 +0545

    fixing version

--- src/core/utils.py
+++ src/core/utils.py
@@ -13,13 +13,13 @@ def save_model(model, name: str, base_dir="../saved_models"):
         torch.save(model, model_path)
     else:
         version = model_version(directory)
-        model_path = os.path.join(directory, f"v{version}.joblib")
+        model_path = os.path.join(directory, f"v{version + 1}.joblib")
         joblib.dump(model, model_path)
 
 
 def model_version(directory: str, ext: str = ".joblib"):
     models = [f for f in os.listdir(directory) if f.endswith(ext)]
     if models:
-        return max([int(m[1:].removesuffix(ext)) for m in models]) + 1
+        return max([int(m[1:].removesuffix(ext)) for m in models])
     else:
-        return 1
+        return 0
