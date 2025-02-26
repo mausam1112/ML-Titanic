91d7a79639a15af9dd820280b5a944752dfb3f18
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-27 01:40:18 +0545

    model configurations added

--- src/core/config.py
+++ src/core/config.py
@@ -1,3 +1,6 @@
 class Configs:
     # model_name: str = "LogisticRegression"
-    model_name: str = "RandomForest"
+    # model_name: str = "RandomForest"
+    model_name: str = "NN"
+    epochs: int = 200
+    batch_size: int = 16
