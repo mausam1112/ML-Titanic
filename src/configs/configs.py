90cdf470b55f8d1ca8df277b59243bc540751011
Mausam Rajbanshi <rajbanshimausam@gmail.com>
2025-02-27 03:37:54 +0545

    renamed constants module to configs, moved config file to configs module, renamed config.py file to configs.py

--- src/configs/configs.py
+++ src/configs/configs.py
@@ -1,6 +1,6 @@
 class Configs:
-    # model_name: str = "LogisticRegression"
+    model_name: str = "LogisticRegression"
     # model_name: str = "RandomForest"
-    model_name: str = "NN"
+    # model_name: str = "NN"
     epochs: int = 200
     batch_size: int = 16
